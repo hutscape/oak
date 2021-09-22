0f7a372c4455604abf04c8991faabb26879125b4
Sayanee <sayanee@gmail.com>
2021-09-22 09:31:50 +0800

    feat: add blinky LED for measuring batt

--- _examples/measure-batt/measure-batt.ino
+++ _examples/measure-batt/measure-batt.ino
@@ -1,8 +1,10 @@
 #define VBATPIN A0
+#define LEDPIN 2
 
 void setup() {
+  pinMode(LEDPIN, OUTPUT);
+
   SerialUSB.begin(9600);
-  while (!SerialUSB) {}
   delay(1000);
 
   SerialUSB.println("Start battery voltage measurement!");
@@ -26,13 +28,22 @@ void loop() {
   measuredvbat *= 2;
 
   // Or the reference voltage E.g. 2.8V, 3.3V, 5V, 3.7V
-  measuredvbat *= 3.7;
+  measuredvbat *= 3.3;
 
   // Measured digital value with multimeter (instead of 1024)
+  // Convert to voltage
   measuredvbat /= 1125;
 
 
   SerialUSB.print("   VBat: ");
   SerialUSB.print(measuredvbat);
   SerialUSB.println("V");
+
+  digitalWrite(LEDPIN, HIGH);
+  SerialUSB.println("HIGH");
+  delay(2000);
+
+  digitalWrite(LEDPIN, LOW);
+  SerialUSB.println("LOW");
+  delay(2000);
 }
