2530422544a2e227e54a83f627dd9238ba9d4321
Sayanee <sayanee@gmail.com>
2021-10-05 15:08:09 +0800

    refactor: fix lora tx code

--- _examples/lora-tx/lora-tx.ino
+++ _examples/lora-tx/lora-tx.ino
@@ -4,16 +4,16 @@
 int counter = 0;
 #define LEDPIN 2
 
-#define RADIO_CS_PIN 5  // D5 on Arduino Zero
-#define RADIO_DI0_PIN 11  // D11 on Arduino Zero
-#define RADIO_RST_PIN 6  // D6 on Arduino Zero
+#define RADIO_CS_PIN 5        // D5 on Arduino Zero
+#define RADIO_DI0_PIN 11      // D11 on Arduino Zero
+#define RADIO_RST_PIN 6       // D6 on Arduino Zero
 #define LORA_FREQUENCY 915E6  // 915 MHz Antenna and LoRa module
 
 void setup() {
   SerialUSB.begin(9600);
   while (!SerialUSB) { }
 
-  SerialUSB.println("LoRa Sender");
+  SerialUSB.println("Starting LoRa transmission...");
   pinMode(LEDPIN, OUTPUT);
 
   LoRa.setPins(RADIO_CS_PIN, RADIO_RST_PIN, RADIO_DI0_PIN);
@@ -34,16 +34,12 @@ void loop() {
   LoRa.endPacket();
 
   counter++;
-  blink(2);
+
   delay(2000);
-}
 
-void blink(int num) {
-  for (int i = 0; i < num; i++) {
-    digitalWrite(LEDPIN, HIGH);
-    delay(250);
+  digitalWrite(LEDPIN, HIGH);
+  delay(250);
 
-    digitalWrite(LEDPIN, LOW);
-    delay(250);
-  }
+  digitalWrite(LEDPIN, LOW);
+  delay(250);
 }
