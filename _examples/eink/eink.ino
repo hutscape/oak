1cbb9f0b718e6a3a0683d50e860c7aa497f18ae2
Sayanee <sayanee@gmail.com>
2021-10-25 14:01:06 +0800

    refactor: new code for eink

--- _examples/eink/eink.ino
+++ _examples/eink/eink.ino
@@ -7,41 +7,41 @@
 #define UNCOLORED 1
 
 unsigned char image[1024];
-Paint paint(image, 0, 0);
+Paint paint(image, 0, 0);  // width should be the multiple of 8
 Epd epd;
 int count = 0;
 char count_string[] = {'0', '0', '0', '\0'};
 
 void setup() {
-  SerialUSB.begin(9600);
-  SerialUSB.println("Starting E-ink display...");
+  Serial.begin(9600);
+
+  Serial.print("e-Paper init ");
+  if (epd.Init(lut_full_update) != 0) {
+    Serial.print("e-Paper init failed");
+    return;
+  }
 }
 
 void loop() {
   sprintf(count_string, "%d", ++count);
   SerialUSB.println(count_string);
 
-  if (epd.Init(lut_full_update) != 0) {
-    SerialUSB.print("e-Paper init failed");
-    return;
-  }
-
-  epd.ClearFrameMemory(0xFF);
+  epd.ClearFrameMemory(0xFF);  // bit set = white, bit reset = black
   epd.DisplayFrame();
-  epd.ClearFrameMemory(0xFF);
+  epd.ClearFrameMemory(0xFF);  // bit set = white, bit reset = black
   epd.DisplayFrame();
 
-  paint.SetRotate(ROTATE_0);
-  paint.SetWidth(200);
-  paint.SetHeight(24);
+  paint.SetWidth(50);
+  paint.SetHeight(120);
+  paint.SetRotate(ROTATE_270);
 
   paint.Clear(COLORED);
-  paint.DrawStringAt(30, 4, count_string, &Font24, UNCOLORED);
-  epd.SetFrameMemory(
-      paint.GetImage(), 0, 50, paint.GetWidth(), paint.GetHeight());
-
+  paint.DrawStringAt(10, 4, "hello", &Font24, UNCOLORED);
+  paint.DrawStringAt(10, 30, count_string, &Font24, UNCOLORED);
+  epd.SetFrameMemory(paint.GetImage(),
+    80, 72, paint.GetWidth(), paint.GetHeight());
   epd.DisplayFrame();
 
-  delay(2000);
+  delay(1000);
   epd.Sleep();
 }
