3aebe3840167848927845bba063772d911f5a05f
Sayanee <sayanee@gmail.com>
2021-11-30 11:42:49 +0800

    refactor: add newlines

--- _examples/gps-fix-parsed/gps-fix-parsed.ino
+++ _examples/gps-fix-parsed/gps-fix-parsed.ino
@@ -34,25 +34,42 @@ void loop() {
   if (millis() - timer > 2000) {
     timer = millis();
     SerialUSB.print("\nTime: ");
-    if (GPS.hour < 10) { SerialUSB.print('0'); }
-    SerialUSB.print(GPS.hour, DEC); SerialUSB.print(':');
-    if (GPS.minute < 10) { SerialUSB.print('0'); }
-    SerialUSB.print(GPS.minute, DEC); SerialUSB.print(':');
-    if (GPS.seconds < 10) { SerialUSB.print('0'); }
-    SerialUSB.print(GPS.seconds, DEC); SerialUSB.print('.');
+
+    if (GPS.hour < 10) {
+      SerialUSB.print('0');
+    }
+    SerialUSB.print(GPS.hour, DEC);
+    SerialUSB.print(':');
+
+    if (GPS.minute < 10) {
+      SerialUSB.print('0');
+    }
+    SerialUSB.print(GPS.minute, DEC);
+    SerialUSB.print(':');
+
+    if (GPS.seconds < 10) {
+      SerialUSB.print('0');
+    }
+    SerialUSB.print(GPS.seconds, DEC);
+    SerialUSB.print('.');
+
     if (GPS.milliseconds < 10) {
       SerialUSB.print("00");
     } else if (GPS.milliseconds > 9 && GPS.milliseconds < 100) {
       SerialUSB.print("0");
     }
+
     SerialUSB.println(GPS.milliseconds);
     SerialUSB.print("Date: ");
-    SerialUSB.print(GPS.day, DEC); SerialUSB.print('/');
-    SerialUSB.print(GPS.month, DEC); SerialUSB.print("/20");
+    SerialUSB.print(GPS.day, DEC);
+    SerialUSB.print('/');
+    SerialUSB.print(GPS.month, DEC);
+    SerialUSB.print("/20");
     SerialUSB.println(GPS.year, DEC);
     SerialUSB.print("Fix: ");
     SerialUSB.print((int)GPS.fix);
-    SerialUSB.print(" quality: "); SerialUSB.println((int)GPS.fixquality);
+    SerialUSB.print(" quality: ");
+    SerialUSB.println((int)GPS.fixquality);
     SerialUSB.print("Time [s] since last fix: ");
     SerialUSB.println(GPS.secondsSinceFix(), 3);
     SerialUSB.print("    since last GPS time: ");
@@ -61,13 +78,19 @@ void loop() {
     SerialUSB.println(GPS.secondsSinceDate(), 3);
     if (GPS.fix) {
       SerialUSB.print("Location: ");
-      SerialUSB.print(GPS.latitude, 4); SerialUSB.print(GPS.lat);
+      SerialUSB.print(GPS.latitude, 4);
+      SerialUSB.print(GPS.lat);
       SerialUSB.print(", ");
-      SerialUSB.print(GPS.longitude, 4); SerialUSB.println(GPS.lon);
-      SerialUSB.print("Speed (knots): "); SerialUSB.println(GPS.speed);
-      SerialUSB.print("Angle: "); SerialUSB.println(GPS.angle);
-      SerialUSB.print("Altitude: "); SerialUSB.println(GPS.altitude);
-      SerialUSB.print("Satellites: "); SerialUSB.println((int)GPS.satellites);
+      SerialUSB.print(GPS.longitude, 4);
+      SerialUSB.println(GPS.lon);
+      SerialUSB.print("Speed (knots): ");
+      SerialUSB.println(GPS.speed);
+      SerialUSB.print("Angle: ");
+      SerialUSB.println(GPS.angle);
+      SerialUSB.print("Altitude: ");
+      SerialUSB.println(GPS.altitude);
+      SerialUSB.print("Satellites: ");
+      SerialUSB.println((int)GPS.satellites);
     }
   }
 }
