96ee3fe497214fa541fe96a49b30e502ec8a8ac6
Sayanee <sayanee@gmail.com>
2021-12-14 16:04:34 +0800

    docs: add tutorial on LoRa GPS duplex example

---
+++ _examples/lora-gps-duplex-a.md
@@ -0,0 +1,35 @@
+---
+layout: example
+title: LoRa GPS duplex node A
+description: Transmit and receive GPS lat-long from a peer LoRa node
+references:
+  - name: Arduino LoRa duplex example code
+    url: https://github.com/sandeepmistry/arduino-LoRa/blob/master/examples/LoRaDuplex/LoRaDuplex.ino
+prerequisites:
+  - name: LoRa transmit
+    url: ./lora-tx
+  - name: LoRa receive
+    url: ./lora-rx
+  - name: LoRa Duplex A
+    url: ./lora-duplex-a
+difficulty: medium
+features:
+  - LoRa
+  - duplex
+  - p2p
+  - GPS
+  - latitude
+  - longitude
+images:
+  prototype: lora-prototype.jpg
+  console: lora-gps-duplex-console.png
+steps:
+  - step: Prepare LoRa GPS duplex B to send and receive GPS information
+  - step: Plug in the Oak PCB to the computer
+  - step: (Option A) Ensure the PCB can be detected with <code>ls -al /dev/cu.usbmodem</code> and <code>arduino-cli board list</code>. Run <code>make</code> to compile and upload the code to the board.
+    image: print-hello-check.png
+  - step: (Option B) Ensure the board can be detected with Arduino IDE. Compile and upload the code to the board.
+    image: arduino-ide-board.png
+---
+
+Run both nodes `AA` and `BB` at the same time to exchange their GPS location. They will not send any GPS data if the location remains the same.
---
+++ _examples/lora-gps-duplex-a/lora-gps-duplex-a.ino
@@ -0,0 +1,106 @@
+#define DEBUG 0
+// 1 to enable verbose debug prints
+// 0 to disable verbose debug prints
+
+#include "src/gps/gps.h"
+#include "src/led/led.h"
+#include "src/lora/lora.h"
+
+byte localAddress = 0xAA;
+byte destinationAddress = 0xBB;
+
+long lastSendTime = 0;
+int interval = 2000;
+
+String incoming = "";
+String gpsTime = "10:00:23";
+String gpsDate = "2021-12-31";
+String latlongInString = "";
+LatLong latlong = {0.00, 0.00};
+LatLong prevLatlong = {0.00, 0.00};
+
+void setup() {
+  SerialUSB.begin(9600);
+  initLED();
+
+  SerialUSB.println("Starting LoRa duplex on node "
+    + String(localAddress, HEX));
+
+  if (!initLoRa()) {
+    SerialUSB.println("Starting LoRa failed!");
+  }
+
+  initGPS();
+}
+
+void loop() {
+  if (millis() - lastSendTime > interval) {
+    if (hasNewGPS(&prevLatlong, &latlong)) {
+      convertLatLongToString(&latlong, latlongInString);
+      sendLoRa(latlongInString, localAddress, destinationAddress);
+
+      SerialUSB.print("\nSending lat-long " + latlongInString);
+      SerialUSB.print(" from 0x" + String(localAddress, HEX));
+      SerialUSB.println(" to 0x" + String(destinationAddress, HEX));
+      SerialUSB.println("Lat/Long: "
+        + String(latlong.latitude, 8)
+        + " / "
+        + String(latlong.longitude, 8));
+
+      prevLatlong = latlong;
+    } else {
+      SerialUSB.println("No new GPS data. Not sending anything over LoRa.");
+    }
+
+    lastSendTime = millis();
+    interval = random(2000) + 1000;
+
+    toggleLED();
+  }
+
+  if (receiveLoRa(LoRa.parsePacket(), localAddress, incoming)) {
+    SerialUSB.println("Received lat-long "
+      + incoming
+      + " from 0x"
+      + String(destinationAddress, HEX)
+      + " to 0x"
+      + String(localAddress, HEX));
+  }
+
+  if (receivedGPSfix()) {
+    getLatLong(&latlong);
+
+    #if DEBUG
+      printGPSinfo();
+    #endif
+  }
+}
+
+void printGPSinfo() {
+  getGPStime(gpsTime);
+  SerialUSB.println("\nTime: " + gpsTime);
+
+  getGPSdate(gpsDate);
+  SerialUSB.println("Date: " + gpsDate);
+
+  SerialUSB.println("Lat/Long: "
+    + String(latlong.latitude, 8)
+    + " / "
+    + String(latlong.longitude, 8));
+
+  SerialUSB.println("GPS Fix? "
+    + (int)getGPSfix());
+  SerialUSB.println("GPS Fix quality: "
+    + (int)getGPSfixquality());
+  SerialUSB.println("Speed (knots): " + String(getGPSspeed()));
+  SerialUSB.println("Angle: " + String(getGPSangle()));
+  SerialUSB.println("Altitude: " + String(getGPSaltitude()));
+  SerialUSB.println("Satellites: " + String(getGPSsatellites()));
+
+  SerialUSB.println("Time [s] since last fix: "
+    + String(getGPStimeSinceLastFix(), 3));
+  SerialUSB.println("    since last GPS time: "
+    + String(getGPSlastTime(), 3));
+  SerialUSB.println("    since last GPS date: "
+    + String(getGPSlastDate(), 3));
+}
