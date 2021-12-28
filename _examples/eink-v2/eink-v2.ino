fc6df3a2eaa97e0d0b2463d3be3d6f52f198aea3
Sayanee <sayanee@gmail.com>
2021-12-28 16:05:11 +0800

    refactor: abstract into initEink and displayOnEink functions

--- _examples/eink-v2/eink-v2.ino
+++ _examples/eink-v2/eink-v2.ino
@@ -15,39 +15,50 @@ char count_string[] = {'0', '0', '0', '\0'};
 void setup() {
   SerialUSB.begin(9600);
   SerialUSB.println("e-Paper init");
-  epd.LDirInit();
-  epd.Clear();
-
-  paint.SetWidth(200);
-  paint.SetHeight(24);
-
-  SerialUSB.println("e-Paper paint");
-  paint.Clear(COLORED);
-  paint.DrawStringAt(30, 4, "Hello world!", &Font16, UNCOLORED);
-  epd.SetFrameMemory(
-    paint.GetImage(), 0, 10, paint.GetWidth(), paint.GetHeight());
-
-  paint.Clear(UNCOLORED);
-  paint.DrawStringAt(30, 4, "e-Paper Demo", &Font16, COLORED);
-  epd.SetFrameMemory(
-    paint.GetImage(), 0, 30, paint.GetWidth(), paint.GetHeight());
-
-  epd.DisplayFrame();
-  delay(2000);
-
-  SerialUSB.println("e-Paper goto sleep");
-  epd.Sleep();
+  initEink();
+  delay(1000);
 }
 
 void loop() {
+  displayOnEink();
+  delay(2000);
+}
+
+bool initEink() {
+  epd.LDirInit();
+  epd.Clear();
+
+  return true;
+}
+
+void displayOnEink() {
   sprintf(count_string, "%d", ++count);
 
-  paint.Clear(UNCOLORED);
-  paint.DrawStringAt(10, 10, count_string, &Font16, COLORED);
-  epd.SetFrameMemoryPartial(
-    paint.GetImage(), 80, 70, paint.GetWidth(), paint.GetHeight());
-  epd.DisplayPartFrame();
-  SerialUSB.println("e-Paper print " + String(count_string));
+  paint.SetWidth(200);
+  paint.SetHeight(24);
+  paint.SetRotate(ROTATE_0);
 
-  delay(2000);
+  paint.Clear(COLORED);
+  paint.DrawStringAt(10, 4, "Lat Long!", &Font16, UNCOLORED);
+  epd.SetFrameMemory(
+      paint.GetImage(), 0, 10, paint.GetWidth(), paint.GetHeight());
+
+  paint.Clear(UNCOLORED);
+  paint.DrawStringAt(10, 4, "at ", &Font16, COLORED);
+  paint.DrawStringAt(40, 4, "00:00", &Font16, COLORED);
+  epd.SetFrameMemory(
+      paint.GetImage(), 0, 30, paint.GetWidth(), paint.GetHeight());
+
+  paint.SetWidth(50);
+  paint.SetHeight(120);
+  paint.SetRotate(ROTATE_270);
+
+  paint.Clear(COLORED);
+  paint.DrawStringAt(12, 10, count_string, &Font16, UNCOLORED);
+  paint.DrawStringAt(12, 32, count_string, &Font16, UNCOLORED);
+  epd.SetFrameMemory(
+      paint.GetImage(), 80, 72, paint.GetWidth(), paint.GetHeight());
+
+  epd.DisplayFrame();
+  SerialUSB.println("e-Paper print " + String(count_string));
 }
